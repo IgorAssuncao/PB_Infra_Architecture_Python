ffb36ea9d4e7cb2cd4fcd5c41b065f01a53c5c09
IgorAssuncao <igor.ormonde@gmail.com>
2019-03-07 20:57:30 -0300

    feature(cliente-servidor): terminado estrutura basica da comunicacao cliente e servidor

--- src/cliente/cliente.py
+++ src/cliente/cliente.py
@@ -2,7 +2,7 @@ import os
 import sys
 import socket
 
-host, port = socket.gethostname(), 9901
+host, port = socket.gethostname(), 9999 
 
 cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -11,3 +11,16 @@ try:
 except Exception as error:
     print(error)
     sys.exit(1)
+
+print(f"""Para sair digite 'fim'""")
+msg = input("""Msg: """)
+cliente.send(msg.encode())
+
+running = True
+while running:
+    resposta = cliente.recv(1024)
+    print(resposta.decode())
+    msg = input("""Msg: """)
+    cliente.send(msg.encode())
+
+cliente.close()
--- src/servidor/servidor.py
+++ src/servidor/servidor.py
@@ -1,2 +1,31 @@
 import os
 import socket
+import time
+
+host = socket.gethostname()
+porta = 9999
+
+tcp_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+tcp_server.bind((host, porta))
+tcp_server.listen()
+
+print(f'Server de nome {host}, esperando conexao na porta {porta}')
+print(f'Caso receba \'fim\' o servidor fechara a conexao')
+
+socket_client, addr = tcp_server.accept()
+print(f'Conectado a: {str(addr)}')
+
+running = True
+while running:
+  msg = socket_client.recv(1024)
+  print(msg.decode())
+  if msg.decode() == 'fim':
+    print('Shutting down connection')
+    socket_client.close()
+    running = False
+  
+  resposta = f"""{host}: {msg}""".encode()
+  socket_client.send(resposta)
+  # time.sleep(1)
+
+tcp_server.close()
